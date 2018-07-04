2cc57c9f1ef53201117c046b4aeb356062b58f06
srishh <srish2698@gmail.com>
2018-07-04 12:22:28 +0530

    first commit

--- app/src/main/java/com/example/ashwani/commotionindia/HomeFragments/HomeWhatsNewFragment.java
+++ app/src/main/java/com/example/ashwani/commotionindia/HomeFragments/HomeWhatsNewFragment.java
@@ -2,6 +2,8 @@ package com.example.ashwani.commotionindia.HomeFragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +24,7 @@ public class HomeWhatsNewFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_whats_new, container, false);
