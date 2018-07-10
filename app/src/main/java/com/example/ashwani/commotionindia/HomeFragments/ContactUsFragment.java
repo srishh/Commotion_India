9b9e996d6e00a32cadc061fb2ceaecaa11021409
srishh <srish2698@gmail.com>
2018-07-10 15:53:45 +0530

    Update on UI

--- app/src/main/java/com/example/ashwani/commotionindia/HomeFragments/ContactUsFragment.java
+++ app/src/main/java/com/example/ashwani/commotionindia/HomeFragments/ContactUsFragment.java
@@ -1,38 +1,30 @@
 package com.example.ashwani.commotionindia.HomeFragments;
 
 
-import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ashwani.commotionindia.R;
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeWhatsNewFragment extends Fragment {
+public class ContactUsFragment extends Fragment {
 
-    public HomeWhatsNewFragment() {
+
+    public ContactUsFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_whats_new, container, false);
-
+        return inflater.inflate(R.layout.form_contact_us, container, false);
     }
 
 }
